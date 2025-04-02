5437e97d2d2a99d8cf5a293b04e5fc4512de5790
Sophie Hinterholzer <167458239+sophiehint@users.noreply.github.com>
2025-04-02 12:29:01 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ from main import calculate_rectangle_area
 
 
 def test_calculate_rectangle_area():
-    assert calculate_rectangle_area(1,2) == 2
+    assert calculate_rectangle_area(1,2) == 0
 
 
 
